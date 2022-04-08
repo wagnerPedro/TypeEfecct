26144c306f94f1bff696f93d64b189ee3233e04d
Wagner shy <Wagnerpedro355@gmail.com>
2022-04-08 13:22:41 +0100

    third
    fiz a função, separar cada caractere do texto passado como argumento, reverti a ordem e guardei o seu retorno na variável char.

--- html/js/type.js
+++ html/js/type.js
@@ -1,3 +1,12 @@
 const DoomElement = document.querySelector("#text");
 const text = 'O javascript é muito interessanta!';
 const interval = 300;
+
+function showText(DoomElement, text, interval) {
+
+    const char = text.split("").reverse();
+
+    console.log(char);
+}
+
+showText(DoomElement, text, interval);
