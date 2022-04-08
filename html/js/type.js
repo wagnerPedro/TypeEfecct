7dfdabfc11330f745b1b9a7f84c79bd0c3716cc9
Wagner shy <Wagnerpedro355@gmail.com>
2022-04-08 13:34:05 +0100

    fourth
    outras funcionalidades necessárias!!

--- html/js/type.js
+++ html/js/type.js
@@ -1,12 +1,24 @@
 const DoomElement = document.querySelector("#text");
-const text = 'O javascript é muito interessanta!';
-const interval = 300;
+const text = 'O javascript é muito interessante!';
+const interval = 200;
 
 function showText(DoomElement, text, interval) {
 
     const char = text.split("").reverse();
 
-    console.log(char);
+    /* intervalo em que cada letra é apresenda, ou mostra no Doom*/
+    const typer = setInterval(() => {
+        
+        if(!char.length) { //Se não existirem mais caracteres no char
+            return clearInterval(typer); //limpa o intervalo, e para a função!
+        }
+
+        //inserindo caracteres no Doom
+        const nextLetter = char.pop(); //pega último elemento
+
+        DoomElement.innerHTML += nextLetter; //mostra no Doom cada letra
+
+    }, interval);
 }
 
 showText(DoomElement, text, interval);
